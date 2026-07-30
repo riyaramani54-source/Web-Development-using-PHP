6eb7fe33ba60a9390b2100fd958817e3e3ca9399
riyaramani54-source <riyaramani54@gmail.com>
2026-07-30 22:09:30 +0530

    Add files via upload

---
+++ que2.php
@@ -0,0 +1,16 @@
+<?php
+
+$array_one = array("One", "Two", "Three")
+
+echo "<pre>";
+print_r($array_one);
+echo "</pre>";
+
+echo "<pre>";
+print_r(array_push($array_one, "Four"));
+echo "</pre>";
+
+echo "<pre>";
+print_r($array_one);
+echo "</pre>";
+?>
