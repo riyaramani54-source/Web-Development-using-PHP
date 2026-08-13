18a6103eee3e54a66562a87a42d6518254f02e63
riyaramani54-source <riyaramani54@gmail.com>
2026-08-13 23:44:38 +0530

    Add files via upload

---
+++ unit-1/que2.php
@@ -0,0 +1,14 @@
+<?php
+// 1.2 Find maximum and minimum number
+
+$a = 25;
+$b = 10;
+$c = 40;
+
+$maximum = max($a, $b, $c);
+$minimum = min($a, $b, $c);
+
+echo "Numbers: $a, $b, $c<br>";
+echo "Maximum number = $maximum<br>";
+echo "Minimum number = $minimum";
+?>
